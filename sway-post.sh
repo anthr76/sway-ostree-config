7f98cb864c29b22559aed5e9ab53b2b0ad2fb3bf
Anthony Rabbito <hello@anthonyrabbito.com>
2022-02-02 06:28:53 -0500

    fix: maybe fix patch
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- sway-post.sh
+++ sway-post.sh
@@ -4,6 +4,7 @@ set -xeuo pipefail
 # Enable SysRQ
 echo 'kernel.sysrq = 1' > /usr/lib/sysctl.d/90-sysrq.conf
 
+ln -sfn /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
 
 # set up PAM for systemd-homed (https://bugzilla.redhat.com/show_bug.cgi?id=1806949)
 patch /etc/pam.d/system-auth <<EOF
@@ -15,13 +16,16 @@ patch /etc/pam.d/system-auth <<EOF
  auth        sufficient    pam_unix.so try_first_pass nullok
 +-auth       sufficient    pam_systemd_home.so  # added
  auth        required      pam_deny.so
+
 -account     required      pam_unix.so
 +account     sufficient    pam_unix.so
 +-account    sufficient    pam_systemd_home.so  # added
+
  password    requisite     pam_pwquality.so try_first_pass local_users_only retry=3 authtok_type=
  password    sufficient    pam_unix.so try_first_pass use_authtok nullok yescrypt shadow
 +-password   sufficient    pam_systemd_home.so  # added
  password    required      pam_deny.so
+
  session     optional      pam_keyinit.so revoke
  session     required      pam_limits.so
 +-session    optional      pam_systemd_home.so  # added
@@ -38,13 +42,16 @@ patch /etc/pam.d/password-auth <<EOF
  auth        sufficient    pam_unix.so try_first_pass nullok
 +-auth       sufficient    pam_systemd_home.so  # added
  auth        required      pam_deny.so
+
 -account     required      pam_unix.so
 +account     sufficient    pam_unix.so
 +-account    sufficient    pam_systemd_home.so  # added
+
  password    requisite     pam_pwquality.so try_first_pass local_users_only retry=3 authtok_type=
  password    sufficient    pam_unix.so try_first_pass use_authtok nullok yescrypt shadow
 +-password   sufficient    pam_systemd_home.so  # added
  password    required      pam_deny.so
+
  session     optional      pam_keyinit.so revoke
  session     required      pam_limits.so
 +-session    optional      pam_systemd_home.so  # added
@@ -57,14 +64,16 @@ EOF
 # "disabled" breaks rpm-ostree (https://bugzilla.redhat.com/show_bug.cgi?id=1882933), so just use permissive
 sed -i 's/SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
 
-ln -sfn /run/systemd/resolve/stub-resolv.conf /etc/resolv.conf
-
 # enable other units
 mkdir -p /usr/lib/systemd/system/getty.target.wants
 ln -s ../getty@.service /usr/lib/systemd/system/getty.target.wants/getty@tty1.service
 ln -s ../systemd-timesyncd.service /usr/lib/systemd/system/sysinit.target.wants/systemd-timesyncd.service
 ln -s ../systemd-resolved.service /usr/lib/systemd/system/multi-user.target.wants/systemd-resolved.service
+ln -s ../systemd-homed.service /usr/lib/systemd/system/multi-user.target.wants/systemd-homed.service
 ln -s ../sshd.socket /usr/lib/systemd/system/sockets.target.wants/sshd.socket
+
+# disable unwanted services
+ln -sfn /dev/null /usr/lib/systemd/user/at-spi-dbus-bus.service
 
 # move OS systemd unit defaults to /usr
 cp -a --verbose /etc/systemd/system /etc/systemd/user /usr/lib/systemd/
